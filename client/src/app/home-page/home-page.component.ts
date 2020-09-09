843785b963029ff39f9d49b2b33c5866f37300d2
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-09-09 04:03:06 -0700

    Added more styling to Home page and ability to navigate from Home page

--- client/src/app/home-page/home-page.component.ts
+++ client/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  handleSearchRedirect() {
+    console.log('Redirecting to search page')
+    this.router.navigateByUrl('/search');
+  }
+  
+  handleSavedRedirect() {
+    console.log('Redirecting to saved page')
+    this.router.navigateByUrl('/saved');
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
