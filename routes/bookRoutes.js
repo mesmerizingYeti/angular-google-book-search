f2c0d8026885646d982cfcc06b6bfacaf8416cfb
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-09-10 03:18:47 -0700

    Changed routes on server to work with proxy

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -4,41 +4,41 @@ const axios = require('axios')
 module.exports = app => {
 
   // Get all books
-  app.get('/books', (req, res) => {
+  app.get('/api/books', (req, res) => {
     Book.find() 
       .then(books => res.json(books))
       .catch(e => console.log(e))
   })
 
   // Get a book
-  app.get('/books/:id', (req, res) => {
+  app.get('/api/books/:id', (req, res) => {
     Book.findOne({ _id: req.params.id })
       .then(book => res.json(book))
       .catch(e => console.log(e))
   })
 
   // Add a book
-  app.post('/books', (req, res) => {
+  app.post('/api/books', (req, res) => {
     Book.create(req.body)
       .then(book => res.json(book))
       .catch(e => console.log(e))
   })
 
   // Update a book
-  app.put('/books/:id', (req, res) => {
+  app.put('/api/books/:id', (req, res) => {
     Book.updateOne({ _id: req.params.id }, req.body )
       .then(() => res.sendStatus(200))
       .catch(e => console.log(e))
   })
 
   // Delete a book
-  app.delete('/books/:id', (req, res) => {
+  app.delete('/api/books/:id', (req, res) => {
     Book.deleteOne({ _id: req.params.id })
       .then(() => res.sendStatus(200))
       .catch(e => console.log(e))
   })
 
-  app.get('/googlebooks/:search', (req, res) => {
+  app.get('/api/googlebooks/:search', (req, res) => {
     axios.get(`https://www.googleapis.com/books/v1/volumes?q=${req.params.search}`)
       .then(({ data }) => res.json(data))
       .catch(e => console.log(e))
